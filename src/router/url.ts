0813c978ab5ecff0020a7d37ea95edc29ba6473e
koki120 <koki20001206@outlook.jp>
2024-06-27 10:03:32 +0900

    fix base path

--- src/router/url.ts
+++ src/router/url.ts
@@ -1,5 +1,5 @@
 export const appUrl = {
-  home: "/strawberry_thinning_annotation_web/",
-  capture: "/strawberry_thinning_annotation_web/capture",
-  captureSave: "/strawberry_thinning_annotation_web/save",
+  home: "",
+  capture: "capture",
+  captureSave: "save",
 };
